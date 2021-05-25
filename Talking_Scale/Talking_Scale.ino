a083ffbccd69522e3d008083cc07798a375d98c6
ashleycox <ash@ashleycox.co.uk>
2021-05-25 16:34:37 +0100

    Update Talking_Scale.ino
    Adds a tare function and optimises the calibration routine

--- Talking_Scale/Talking_Scale.ino
+++ Talking_Scale/Talking_Scale.ino
@@ -10,7 +10,7 @@
 #include <talkie.h>
 #include "HX711.h"
 
-//Program variables
+//Globalvariables
 const int LOADCELL_DOUT_PIN = 4; //HX711 digital out
 const int LOADCELL_SCK_PIN = 5; //HX711 CLK
 int r1; //Number before decimal
@@ -19,8 +19,11 @@ int r3; //the second decimal
 float cal_factor; //Scale calibration factor, set by calibration function
 float reading; //Scale Measurement
 const int buttonPin = 2; //Calibration button pin
-int calPressed = 0; //is the button pressed
 int calibrated = 1; //used for the innitial tare on power up
+unsigned long previousTenthSecondMillis = 0L;
+long tenthSecond = 100UL;
+byte buttonStillDown = 0;
+
 
 //Speech variables
 const uint8_t spGRAMS[] PROGMEM = {};
@@ -38,115 +41,127 @@ const uint8_t spPOINT[] PROGMEM = {0x0A, 0xC8, 0x2E, 0x8C, 0x01, 0xD1, 0x85, 0x9
 const uint8_t spSTART[] PROGMEM = {0x02, 0xF8, 0x49, 0xCC, 0x00, 0xBF, 0x87, 0x3B, 0xE0, 0xB7, 0x60, 0x03, 0xFC, 0x9A, 0xAA, 0x80, 0x3F, 0x92, 0x11, 0x30, 0x29, 0x9A, 0x02, 0x86, 0x34, 0x5F, 0x65, 0x13, 0x69, 0xE2, 0xDA, 0x65, 0x35, 0x59, 0x8F, 0x49, 0x59, 0x97, 0xD5, 0x65, 0x7D, 0x29, 0xA5, 0xDE, 0x56, 0x97, 0xF5, 0x85, 0x8E, 0xE4, 0x5D, 0x6D, 0x0E, 0x23, 0x39, 0xDC, 0x79, 0xD4, 0xA5, 0x35, 0x75, 0x72, 0xEF, 0x51, 0x95, 0xE9, 0x38, 0xE6, 0xB9, 0x4B, 0x5D, 0x1A, 0x26, 0x6B, 0x3B, 0x46, 0xE0, 0x14, 0xA5, 0x2A, 0x54, 0x03, 0x40, 0x01, 0x43, 0xBA, 0x31, 0x60, 0x73, 0x35, 0x04, 0x4E, 0x51, 0xAA, 0x42, 0x35, 0xFE, 0x1F};
 const uint8_t spREADY[] PROGMEM = {0x6A, 0xB4, 0xD9, 0x25, 0x4A, 0xE5, 0xDB, 0xD9, 0x8D, 0xB1, 0xB2, 0x45, 0x9A, 0xF6, 0xD8, 0x9F, 0xAE, 0x26, 0xD7, 0x30, 0xED, 0x72, 0xDA, 0x9E, 0xCD, 0x9C, 0x6D, 0xC9, 0x6D, 0x76, 0xED, 0xFA, 0xE1, 0x93, 0x8D, 0xAD, 0x51, 0x1F, 0xC7, 0xD8, 0x13, 0x8B, 0x5A, 0x3F, 0x99, 0x4B, 0x39, 0x7A, 0x13, 0xE2, 0xE8, 0x3B, 0xF5, 0xCA, 0x77, 0x7E, 0xC2, 0xDB, 0x2B, 0x8A, 0xC7, 0xD6, 0xFA, 0x7F,};
 const uint8_t spERROR[] PROGMEM = {0x6D, 0x6C, 0x6E, 0x89, 0x5B, 0x36, 0x8F, 0xA9, 0x9B, 0x11, 0x5A, 0xE9, 0x33, 0xA7, 0x31, 0x17, 0xB7, 0x4A, 0x47, 0xFD, 0xC6, 0x92, 0x9A, 0x8B, 0x2F, 0x65, 0x4B, 0x6B, 0x1C, 0xE4, 0xD5, 0xD8, 0x2D, 0xAF, 0x65, 0x8D, 0x83, 0xAD, 0x9A, 0xB2, 0x95, 0x23, 0x76, 0x93, 0x58, 0xCA, 0xD7, 0xCE, 0xEC, 0x57, 0xF8, 0xD8, 0x5B, 0x3A, 0x8B, 0x3D, 0xC5, 0xE7, 0x7C, 0xE9, 0xCD, 0xBD, 0x30, 0x86, 0xDA, 0x86, 0x2F, 0x97, 0x82, 0x38, 0xEC, 0x13, 0xFE, 0xE4, 0x07, 0xED, 0x35, 0x4B, 0xF8, 0x73, 0x4E, 0xEC, 0xD2, 0x3C, 0xEE, 0x2F, 0xCB, 0xB9, 0x9D, 0x3B, 0xFF, 0x3F};
+const uint8_t spMINUS[] PROGMEM = {0x6A,0xCE,0x8C,0xF4,0xD4,0x93,0x86,0xA5,0x49,0xE2,0x31,0x73,0x1B,0x96,0x26,0x48,0xC6,0x25,0x59,0x9A,0x1B,0x67,0xEC,0xD4,0x34,0x69,0xEA,0x16,0x31,0x33,0x92,0x9C,0xA1,0x88,0xEC,0x08,0xEA,0x72,0xBA,0x2A,0xA6,0x2D,0xB4,0xCB,0xC9,0xBA,0x19,0xB5,0xD4,0x35,0x27,0xEB,0xA9,0x29,0xCC,0x67,0x9F,0xAC,0x65,0xE7,0xE0,0x9E,0x32,0xAA,0x11,0xD1,0xCD,0xC3,0x4D,0x6B,0x7B,0xA0,0x54,0xF5,0x4C,0x67,0x68,0xA1,0xC4,0xC4,0x17,0x8F,0xA5,0x46,0x37,0x97,0x5E,0xD2,0xAE,0x12,0xDD,0x5D,0x7A,0x89,0xBB,0x72,0x72,0xF5,0xD8,0xD9,0x02,0xA8,0xB2,0x58,0x01,0xDF,0x9B,0x09,0xE0,0xB7,0x74,0x01,0xFC,0xE6,0xA9,0x80,0x9F,0x52,0x05,0xF0,0x67,0xFB,0xFF,0x03};
+
 
 //Initialise libraries
 Talkie voice;
 HX711 scale;
 
 void setup() {
-  
   //Initialise calibration button with onboard pullup resistor
   pinMode(buttonPin, INPUT_PULLUP);
 
-  //initalise the HX711
+  //initialise the HX711
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-if(EEPROM.readFloat(0)) {
-cal_factor = EEPROM.readFloat(0);
-scale.set_scale(cal_factor);
-} else {
-scale.set_scale(0);
-}
-        voice.say(spREADY);
+  //check if we have a calibration factor stored in our EEPROM
+  if (EEPROM.readFloat(0)) {
+    //if we do, we want to use it
+    cal_factor = EEPROM.readFloat(0);
+    scale.set_scale(cal_factor);
+  } else {
+    //we'll need to calibrate, so use zero for now
+    scale.set_scale(0);
+  }
+  voice.say(spREADY);
 }
 
 void loop() {
-  //detects when the calibration button is held
-  while (digitalRead(buttonPin) == LOW ) {
-    calPressed = 1;
+  // do this every 100ms
+  if (millis() - previousTenthSecondMillis >= tenthSecond) {
+    if (!digitalRead(buttonPin)) {
+      if ((buttonStillDown++ > 0 ) && (buttonStillDown < 3)) {
+        scale.tare();
+        voice.say(spZERO);
+              } else {
+        if (buttonStillDown > 10 ) {
+          calibration();
+        }
+      }
+    } else {
+      buttonStillDown = 0;
+      measurement();
+    }
   }
-  if (calPressed == 0) {
-    measurement();
+    previousTenthSecondMillis += tenthSecond;
   }
-  else if (calPressed == 1) {
-    //reset so we don't run calibration indefinitely
-    calPressed = 0;
-    calibration();
-  }
-}
 
-void measurement() {
+  void measurement() {
     //We don't want to tare if we just calibrated
-    if(calibrated == 1) {
+    if (calibrated == 1) {
       calibrated = 0;
       scale.tare();
     }
     //Take an average of 20 readings from the HX711 for better accuracy
-  reading = scale.get_units(20), 2;
-  //We break the reading up to speak it properly
-  //R1 is an integer, therefore we lose the decimal
-  r1 = reading;
-  //Work out the decimal
-  r2 = reading * 10 - r1 * 10;
-  //A second decimal
-  r3 = reading / r1;
-  //Speak the reading
-  speakNum(r1);
-  voice.say(spPOINT);
-  speakNum(r2);
-  speakNum(r3);
-  //Put the HX711 in sleep mode
-  scale.power_down();
-  //add a second between the spoken values
-  delay(1000);
-  //Power up the HX711
-  scale.power_up();
-}
-
-void calibration () {
-  //Zero the scale
-  scale.set_scale();
-  scale.tare();
-  voice.say(spSTART);
-  //gives us time to place the weight
-  delay (2000);
-  //Populate our calibration factor variable
-//using a 5 gram known weight
-cal_factor = scale.get_units(20) / 5.00;
-  scale.set_scale(cal_factor);
-if(EEPROM.readFloat(0)) {
-  EEPROM.updateFloat(0, cal_factor);
-} else {
-  EEPROM.writeFloat(0, cal_factor);
-}
-    delay(100);
-  voice.say(spFIVE);
-  calibrated = 0;
-  measurement();
-}
-
-//simple function to translate the numbers 1-9 and 0 into spoken values
-void speakNum(int x) {
-  if (x == 1) {
-    voice.say(spONE);
-  } else if (x == 2) {
-    voice.say(spTWO);
-  } else if (x == 3) {
-    voice.say(spTHREE);
-  } else if (x == 4) {
-    voice.say(spFOUR);
-  } else if (x == 5) {
-    voice.say (spFIVE);
-  } else if (x == 6) {
-    voice.say(spSIX);
-  } else if (x == 7) {
-    voice.say(spSEVEN);
-  } else if (x == 8) {
-    voice.say(spEIGHT);
-  } else if (x == 9) {
-    voice.say(spNINE);
-  } else if (x == 0) {
-    voice.say(spZERO);
-  } else {
-    voice.say(spERROR);
+    reading = scale.get_units(20), 2;
+    //We break the reading up to speak it properly
+    //R1 is an integer, therefore we lose the decimal
+    r1 = reading;
+    //Work out the decimal
+    r2 = reading * 10 - r1 * 10;
+    //A second decimal
+    r3 = reading / r1;
+    //Speak the reading
+    speakNum(r1);
+    voice.say(spPOINT);
+    speakNum(r2);
+    speakNum(r3);
+    //Put the HX711 in sleep mode
+    scale.power_down();
+    //add a second between the spoken values
+    delay(1000);
+    //Power up the HX711
+    scale.power_up();
   }
-}
+
+  void calibration () {
+    //Zero the scale
+    scale.set_scale();
+    scale.tare();
+    voice.say(spSTART);
+    //gives us time to place the weight
+    delay (2000);
+    //Populate our calibration factor variable
+    //using a 5 gram known weight
+    cal_factor = scale.get_units(20) / 5.00;
+    scale.set_scale(cal_factor);
+    //store the calibration factor in EEPROM
+    if (EEPROM.readFloat(0)) {
+      EEPROM.updateFloat(0, cal_factor);
+    } else {
+      EEPROM.writeFloat(0, cal_factor);
+    }
+    delay(100);
+    voice.say(spFIVE);
+    calibrated = 0;
+    measurement();
+  }
+
+  //simple function to translate the numbers 1-9 and 0 into spoken values
+  void speakNum(int x) {
+    if(x <0) {
+      voice.say(spMINUS);
+    } else if (x == 1) {
+          voice.say(spONE);
+    } else if (x == 2) {
+      voice.say(spTWO);
+    } else if (x == 3) {
+      voice.say(spTHREE);
+    } else if (x == 4) {
+      voice.say(spFOUR);
+    } else if (x == 5) {
+      voice.say (spFIVE);
+    } else if (x == 6) {
+      voice.say(spSIX);
+    } else if (x == 7) {
+      voice.say(spSEVEN);
+    } else if (x == 8) {
+      voice.say(spEIGHT);
+    } else if (x == 9) {
+      voice.say(spNINE);
+    } else if (x == 0) {
+      voice.say(spZERO);
+    } else {
+      voice.say(spERROR);
+    }
+  }
